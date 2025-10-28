73468b16d825f9e027bbea3f1199d002e3578d89
Samuel R. Martin <samuelruizmartin2412@gmail.com>
2025-10-28 14:44:04 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,101 @@
+public class App {
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RED = "\u001B[31m";
+
+    public static void main(String[] args) throws Exception {
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+
+        int max = 0;
+        int med = 0;
+        int min = 0;
+        boolean correcto = false;
+        do {
+            try {
+                System.out.printf("%n");
+                num1 = Integer.parseInt(System.console().readLine("Introduzca num 1 entero: "));
+                System.out.printf("%n");
+                num2 = Integer.parseInt(System.console().readLine("Introduzca num 2 entero: "));
+                System.out.printf("%n");
+                num3 = Integer.parseInt(System.console().readLine("Introduzca num 3 entero: "));
+                correcto = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Debes introducir un numero entero");
+                correcto = false;
+            } catch (Exception e) {
+                System.out.println("Se a prodicido un Error Inesperado, Intentelo de nuevo");
+                correcto = false;
+            }
+        } while (!correcto);
+
+        max = Math.max(Math.max(num1, num2), Math.max(num2, num3));
+        min = Math.min(Math.min(num1, num2), Math.min(num2, num3));
+
+        if (num1 != max && num1 != min)
+            med = num1;
+        else if (num2 != max && num2 != min)
+            med = num2;
+        else if (num3 != max && num3 != min)
+            med = num3;
+
+        System.out.println("""
+
+                1. Oricontal
+
+                2. Vertical
+                """);
+        int opcion = Integer.parseInt(System.console().readLine("Introduzca la opción: "));
+        System.out.println();
+
+        if (opcion == 1) {
+            for (int i = 1; i <= 3; i++) {
+                System.out.printf("num %s ", i);
+                for (int j = 0; j < ((i == 1) ? num1 : (i == 2) ? num2 : num3); j++) {
+
+                    if (max == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
+                        System.out.print(GREEN + "█" + RESET);
+                    }
+                    if (med == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
+                        System.out.print(YELLOW + "█" + RESET);
+                    }
+                    if (min == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
+                        System.out.print(RED + "█" + RESET);
+                    }
+                }
+                System.out.printf("%n%n");
+            }
+        }
+        // System.out.println("""
+        // 1 2 3
+        // ####
+        // #### ####
+        // #### #### ####
+        // num1 num2 num3
+        // """);
+
+        if (opcion == 2) {
+            for (int i = 0; i <= max + 2; i++) {
+                System.out.println();
+                for (int j = 1; j <= 3; j++) {
+                    if (i == 0) {
+                        System.out.printf("%-4d ", ((i == 1) ? num1 : (i == 2) ? num2 : num3));
+                    } else if (i != max + 2) {
+                        System.out.printf("%-2s%2s%2s "," ", j, " ");
+
+                    } else if (i == max + 2) {
+                        System.out.printf("%-4s ", " num "+j);
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.printf("%s %d %s %d %s %d%s", GREEN + "Maximo", max, YELLOW + "Medio", med, RED + "Bajo", min,
+                " " + RESET);
+
+    }
+
+}
