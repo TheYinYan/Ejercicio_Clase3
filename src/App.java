6046516d5aa9705298d34a12afeab38760170a2e
Samuel R. Martin <samuelruizmartin2412@gmail.com>
2025-10-31 10:04:31 +0100

    j

--- src/App.java
+++ src/App.java
@@ -43,7 +43,7 @@ public class App {
 
         System.out.println("""
 
-                1. Horicontal
+                1. Horizontal
 
                 2. Vertical
 
@@ -56,7 +56,7 @@ public class App {
             System.out.println();
             System.out.println("Versión Horizontal");
             System.out.println();
-
+            
             for (int i = 1; i <= 3; i++) {
                 System.out.printf("num %s ", i);
                 for (int j = 0; j < ((i == 1) ? num1 : (i == 2) ? num2 : num3); j++) {
@@ -64,10 +64,10 @@ public class App {
                     if (max == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
                         System.out.print(GREEN + "█" + RESET);
                     }
-                    if (med == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
+                    else if (med == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
                         System.out.print(YELLOW + "█" + RESET);
                     }
-                    if (min == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
+                    else if (min == ((i == 1) ? num1 : (i == 2) ? num2 : num3)) {
                         System.out.print(RED + "█" + RESET);
                     }
                 }
@@ -87,13 +87,11 @@ public class App {
             System.out.println("Versión Vertical");
             System.out.println();
 
-            for (int i = max + 1; i >= 1; i--) {
+            System.out.printf("%7d%7d%7d%n", num1, num2, num3);
+            for (int i = max; i >= 1; i--) {
                 for (int j = 1; j <= 3; j++) {
                     int valor = (j == 1) ? num1 : (j == 2) ? num2 : num3;
-                    int valor1 = (j == 1) ? num1 + 1 : (j == 2) ? num2 + 1 : num3 + 1;
-                    if (i == valor1) {
-                        System.out.printf("%7d", valor);
-                    } else if (i < valor1) {
+                    if (i <= valor) {
                         if (valor == max)
                             System.out.printf(GREEN + "%7s" + RESET, "████");
                         else if (valor == med)
